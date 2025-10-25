634b854ac863906b7dc5873f65b664ddadba7ecb
Mew Orapin <youxrmex@gmail.com>
2025-10-25 11:11:41 +0700

    Daily Vocab

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -4,7 +4,7 @@ import { words } from "@/data/words";
 export async function GET() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const word = words[randomIndex];
-  console.log('Rose was here...');
+  console.log('Mew was here...');
   return NextResponse.json({ data: word });
 }
 
